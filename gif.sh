752fab4af421c39c2dda637d5d43725f36b62be3
Yan Babitski <yan.babitski@gmail.com>
2020-07-06 23:43:04 -0700

    Fix the bug where leftovers from previous animations were used

--- gif.sh
+++ gif.sh
@@ -5,6 +5,7 @@ FILE=$1
 RESOLUTION=$2
 SCENE=$3
 
+rm -f media/videos/$FILE/$RESOLUTION/frames/*
 mkdir -p media/videos/$FILE/$RESOLUTION/frames
 ffmpeg -i media/videos/$FILE/$RESOLUTION/$SCENE.mp4 media/videos/$FILE/$RESOLUTION/frames/frame%04d.png
 gifski -o media/videos/$FILE/$RESOLUTION/$SCENE.gif media/videos/$FILE/$RESOLUTION/frames/frame*.png
